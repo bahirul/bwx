c94ee17ecc7bf7181be48467f703c924c5310388
Ari <ari@detelnetworks.id>
2024-10-25 14:40:16 +0800

    update bin

--- bin/create-bwx.mjs
+++ bin/create-bwx.mjs
@@ -70,7 +70,7 @@ const excludeDevDependencies = [];
     }
 
     const gitignorePath = makePath(destination, '.gitignore');
-    await fs.writeFile(gitignorePath, `${gitignoreContent}\n# src/config/main.ts\n./src/config/main.ts\n`);
+    await fs.writeFile(gitignorePath, `${gitignoreContent}\n# App configuration\n./src/config/main.ts\n`);
 
     // edit package.json
     const pkgPath = makePath(destination, 'package.json');
