fc2b65440bee5f6a362b94d7ff7271004419468c
Ari <ari@detelnetworks.id>
2024-11-26 14:51:44 +0800

    update config

--- src/config/app.ts
+++ src/config/app.ts
@@ -13,30 +13,9 @@ const appConfig = {
     id: process.env.APP_ID || 'app',
     port: parseInt(process.env.APP_PORT || '3000'),
 
-    // jwt configuration
-    jwt: {
-        algo: process.env.JWT_ALGO || 'HS256',
-        secret: process.env.JWT_SECRET || 'randomSecretOf32CharsPleaseChange',
-    },
-
-    // auth expiration
-    auth: {
-        expiration: {
-            token: parseInt(process.env.AUTH_TOKEN_EXP || '300'), // 5 minutes
-            refresh: parseInt(process.env.AUTH_REFRESH_EXP || '86400'), // 1 day
-        },
-    },
-
     // log configuration
     log: {
         level: process.env.LOG_LEVEL || 'info',
-    },
-
-    // redis configuration
-    redis: {
-        host: process.env.REDIS_HOST || 'localhost',
-        port: parseInt(process.env.REDIS_PORT || '6379', 10),
-        password: process.env.REDIS_PASSWORD || '',
     },
 };
 
